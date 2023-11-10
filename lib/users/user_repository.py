bde45d431d0567cc0b1d027cc8db3bf5bf74edf2
Keziah <keziah.qrsh@gmail.com>
2023-11-10 10:36:08 +0000

    created the signup login

--- lib/users/user_repository.py
+++ lib/users/user_repository.py
@@ -1,14 +1,29 @@
-from lib.users import *
-
+from lib.users import User
+from flask import request
 class User_Repository:
     def __init__(self, connection):
         self._connection = connection
         
-    def create():
+    def create(self, request):
         """
         checks for uniquness, throws error code for username or email already exists
         """
-        pass
+        if len(self._connection.execute(
+            "SELECT * FROM users WHERE username=%s", [request.form.get("username")])) > 0:
+            return "User already exists", 409
+        else:
+            user = User(
+            request.form.get("username"),
+            request.form.get("email"),
+            request.form.get("password"),
+        )
+            self._connection.execute(
+            "INSERT INTO users "
+            + "(username, email, password) "
+            + "VALUES (%s, %s, %s,)",
+            [user.username, user.email, user.password],
+        )
+        
     def login():
         pass
     
